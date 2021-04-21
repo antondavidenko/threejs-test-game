5ccdf2cef9a9ef83b05610484155aa41992facb5
antondavidenko <anton.davidenko@gmail.com>
2021-04-21 19:47:22 +0300

    add trees;

---
+++ src/components/world/utils/add-trees.ts
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+import { loadFBX } from '@src/utils/loadFBX';
+
+export async function addTrees(scene: THREE.Scene) {
+  const forest: THREE.Group = await loadFBX('models/forest.FBX');
+  const pine1 = forest.getObjectByName('Fir_v1_1').clone();
+  scene.add(pine1);
+  pine1.position.x = 5;
+  pine1.position.z = 0;
+  const pine2 = forest.getObjectByName('Fir_v1_2').clone();
+  scene.add(pine2);
+  pine2.position.x = -5;
+  pine2.position.z = 0;
+
+  const map = ((pine1 as THREE.Mesh).material as any).map;
+  const newMaterial = new THREE.MeshBasicMaterial({ map });
+
+  (pine1 as THREE.Mesh).material = newMaterial;
+  (pine1 as THREE.Mesh).castShadow = true;
+  (pine1 as THREE.Mesh).receiveShadow = true;
+
+  (pine2 as THREE.Mesh).material = newMaterial;
+  (pine2 as THREE.Mesh).castShadow = true;
+  (pine2 as THREE.Mesh).receiveShadow = true;
+}
--- src/components/world/world.component.ts
+++ src/components/world/world.component.ts
@@ -1,39 +1,41 @@
 import * as THREE from 'three';
+import { addTrees } from './utils/add-trees';
 import { addSkyBox } from './utils/addSkyBox';
 import { createTerrainLayer } from './utils/CreateTerrainLayer';
 
 export const GROUND_SCALE = 200;
 
 function getTexture(fileName: string): string {
-    return `textures/panorama/${fileName}.jpg`;
+  return `textures/panorama/${fileName}.jpg`;
 }
 
 function planeMaterial(fileName: string): THREE.MeshPhongMaterial {
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    material.map = new THREE.TextureLoader().load(getTexture(fileName));
-    material.map.repeat.set(10, 10);
-    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-    return material;
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  material.map = new THREE.TextureLoader().load(getTexture(fileName));
+  material.map.repeat.set(10, 10);
+  material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+  return material;
 }
 
 function waterMaterial(fileName: string): THREE.MeshPhongMaterial {
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    material.map = new THREE.TextureLoader().load(getTexture(fileName));
-    material.map.repeat.set(10, 10);
-    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-    material.transparent = true;
-    material.opacity = 0.75;
-    return material;
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  material.map = new THREE.TextureLoader().load(getTexture(fileName));
+  material.map.repeat.set(10, 10);
+  material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+  material.transparent = true;
+  material.opacity = 0.75;
+  return material;
 }
 
 export class WorldComponent {
 
-    constructor(private scene: THREE.Scene) {
-        addSkyBox(this.scene);
-        createTerrainLayer(this.scene, getTexture('map_ground'), planeMaterial('ground'), -32.4, 64);
-        createTerrainLayer(this.scene, getTexture('map_rock'), planeMaterial('rock'), -32.6, 64);
-        createTerrainLayer(this.scene, getTexture('map_sand'), planeMaterial('sand'), -32.42, 64);
-        createTerrainLayer(this.scene, getTexture('map_sand'), waterMaterial('water'), -1, 0);
-    }
+  constructor(private scene: THREE.Scene) {
+    addSkyBox(this.scene);
+    createTerrainLayer(this.scene, getTexture('map_ground'), planeMaterial('ground'), -32.4, 64);
+    createTerrainLayer(this.scene, getTexture('map_rock'), planeMaterial('rock'), -32.6, 64);
+    createTerrainLayer(this.scene, getTexture('map_sand'), planeMaterial('sand'), -32.42, 64);
+    createTerrainLayer(this.scene, getTexture('map_sand'), waterMaterial('water'), -1, 0);
+    addTrees(this.scene);
+  }
 
 }
